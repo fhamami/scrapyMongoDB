3eccbe5c7ecc8749b7a7b6ff12f4e66b9eaf3449
fhamami <fuadgoinet@gmail.com>
2016-09-28 16:15:19 +0700

    avoid duclicate content

--- pipelines.py
+++ pipelines.py
@@ -14,23 +14,32 @@ from scrapy import log
 class MongoDBPipeline(object):
 
 	def __init__(self):
-			connection = pymongo.MongoClient(
-				settings['MONGODB_SERVER']
-				#settings['MONGODB_PORT']
-			)
-			db = connection[settings['MONGODB_DB']]
-			self.collection = db[settings['MONGODB_COLLECTION']]
+		connection = pymongo.MongoClient(
+			settings['MONGODB_SERVER']
+			#settings['MONGODB_PORT']
+		)
+		db = connection[settings['MONGODB_DB']]
+		self.collection = db[settings['MONGODB_COLLECTION']]
 
 	def process_item(self, item, spider):
-		valid = True
+		# valid = True
+		# for data in item:
+		# 	if not data:
+		# 		valid = False
+		# 		raise DropItem("Missing {0}!".format(data))
+		# if valid:
+		# 	self.collection.insert(dict(item))
+		# 	log.msg("Question added to MongoDB database!", level=log.DEBUG, spider=spider)
+
+		# return item
+
+		# Avoid adding duplicate questions to the database. using a MongoDB upsert
+
 		for data in item:
 			if not data:
-				valid = False
-				raise DropItem("Missing {0}!".format(data))
-		if valid:
-			self.collection.insert(dict(item))
-			log.msg("Question added to MongoDB database!", level=log.DEBUG, spider=spider)
-
+				raise DropItem("Missing data!")
+		self.collection.update({'url': item['url']}, dict(item), upsert=True)
+		log.msg("Question added to MongoDB database!", level=log.DEBUG, spider=spider)
 		return item
 
     # def process_item(self, item, spider):
